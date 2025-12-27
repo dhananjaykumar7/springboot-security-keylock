c95c5f56fe40e29b8e6ec3e0b8c6b5d54ffdf4a8
dhananjay <dhananjay.cdac@gmail.com>
2025-12-27 09:16:56 +0530

    Fix for Spring Security with KeyLock implementation

---
+++ src/main/java/com/example/api_gateway_demo/ApiGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_gateway_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_gateway_demo/ApiGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_gateway_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
